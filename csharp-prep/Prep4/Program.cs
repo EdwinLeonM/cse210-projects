40d435f45eedc2c272cded4ce83f030f3bb49d0b
EdwinLeonM <leonardo.byu@gmail.com>
2024-04-27 18:28:22 -0500

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.Write("Enter a list of numbers, type 0 when finished. \n");
+        List<int> numbers = new List<int>();
+    
+        int userNumber = -1;
+        while (userNumber != 0)   
+        {
+        
+            Console.Write("Enter number:");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+
+        }
+        
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        // Stretch Challenge   
+
+        Console.WriteLine($"The largest number is: {max}");
+
+        int min = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number < min && number > 0)
+            {
+                min = number;
+
+            }
+        }
+
+        Console.WriteLine($"The smallest positive number is: {min}");
+
+        Console.WriteLine("The sorted list is:");
+        for (int i = 0; i < numbers.Count; i++)
+        {
+        Console.WriteLine(numbers[i]);
+        }
+
     }
 }
