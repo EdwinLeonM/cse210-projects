98d1e57b09de19f31fbac519cdb2d9fe71016d15
EdwinLeonM <leonardo.byu@gmail.com>
2024-06-01 18:41:50 -0500

    finished foundation2

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,45 @@
+class Order
+{
+    private Customer _customer;
+    private List<Product> _products;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double totalCost = 0;
+        foreach (var product in _products)
+        {
+            totalCost += product.GetTotalCost();
+        }
+        double shippingCost = _customer.IsInUSA() ? 5 : 35;
+        return totalCost + shippingCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (var product in _products)
+        {
+            label += product.GetProductInfo() + "\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        string label = "Shipping Label:\n";
+        label += _customer.GetName() + "\n";
+        label += _customer.GetAddress();
+        return label;
+    }
+}
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,36 @@
+
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+ 
+        Address address1 = new Address("123 Main St", "Anytown", "CA", "USA");
+        Address address2 = new Address("456 Elm St", "Othertown", "ON", "Canada");
+
+        Customer customer1 = new Customer("John Doe", address1);
+        Customer customer2 = new Customer("Jane Smith", address2);
+
+        Product product1 = new Product("Widget", "W123", 3.50, 4);
+        Product product2 = new Product("Gadget", "G456", 15.00, 2);
+        Product product3 = new Product("Thingamajig", "T789", 9.99, 5);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product2);
+        order2.AddProduct(product3);
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.GetTotalCost():0.00}\n");
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.GetTotalCost():0.00}");
     }
 }
